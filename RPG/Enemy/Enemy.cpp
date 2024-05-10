c8db34c9181b5eca1ee11e11b2f0e33231688994
SebastianGS691 <sebasgsalazar18@gmail.com>
2024-05-09 21:01:11 -0600

    Se cambia el string a Char

---
+++ RPG/Enemy/Enemy.cpp
@@ -0,0 +1,75 @@
+//
+// Created by Victor Navarro on 13/02/24.
+//
+#define GREEN   "\033[32m"
+#define RESET   "\033[0m"
+#include "Enemy.h"
+#include <iostream>
+
+using namespace std;
+
+//TODO: Check the circular dependency
+int getRolledAttack(int attack) {
+    int lowerLimit = attack * .80;
+    return (rand() % (attack - lowerLimit)) + lowerLimit;
+}
+
+Enemy::Enemy(char name[], int health, int attack, int defense, int speed) : Character(name, health, attack, defense, speed, false) {
+};
+
+void Enemy::flee() {
+    this->fleed = true;
+}
+
+void Enemy::doAttack(Character *target) {
+    int rolledAttack = getRolledAttack(getAttack());
+    int trueDamage = target->getDefense() > rolledAttack ? 0 : rolledAttack - target->getDefense();
+    target->takeDamage(trueDamage);
+}
+
+void Enemy::takeDamage(int damage) {
+    setHealth(getHealth() - damage);
+    if(getHealth() <= 0) {
+        cout<<GREEN<<getName()<<" has died"<<RESET<< endl;
+    }
+    else {
+        cout<<getName()<<" has taken " << damage << " damage" << endl;
+    }
+}
+
+Character* Enemy::getTarget(vector<Player *> teamMembers) {
+    
+    // Obtiene el miembro del equipo con menos vida
+    int targetIndex = 0;
+    int lowestHealth = INT_MAX;
+    for(int i=0; i < teamMembers.size(); i++) {
+        if(teamMembers[i]->getHealth() < lowestHealth) {
+            lowestHealth = teamMembers[i]->getHealth();
+            targetIndex = i;
+        }
+    }
+
+    return teamMembers[targetIndex];
+}
+//Accion de ataque del enemigo
+Action Enemy::takeAction(vector<Player *> player) {
+    Action myAction;
+    myAction.speed = getSpeed();
+    myAction.subscriber = this;
+	Character* target = getTarget(player);
+    if (getHealth() < maxHealth * 0.60 && rand() % 100 < 50) { //Cuando tiene menos del 60% de vida hay un 50% de que huya
+		myAction.action = [this, player]() {
+			flee();
+		};
+    }
+    else {
+		myAction.target = target;
+		myAction.action = [this, target]() {
+            doAttack(target);
+		};
+    }
+
+    return myAction;
+}
+
+//samlo estuvo aqui >:)
---
+++ RPG/Player/Player.cpp
@@ -0,0 +1,125 @@
+//
+// Created by Victor Navarro on 13/02/24.
+//
+#include "Player.h"
+#include <iostream>
+#include "../Utils.h"
+#include <algorithm>
+#define RED "\033[31m"
+#define RESET "\033[0m"
+#define YELLOW  "\033[33m"
+#define GREEN   "\033[32m"
+#define BLUE    "\033[34m"
+
+using namespace std;
+using namespace combat_utils;
+
+bool compareSpeed(Enemy *a, Enemy *b) {
+    return a->getSpeed() > b->getSpeed();
+}
+
+Player::Player(char name, int health, int attack, int defense, int speed) : Character(name, health, attack, defense, speed, true) {
+    experience = 0;
+    level = 1;
+}
+
+void Player::doAttack(Character *target) {
+    int rolledAttack = getRolledAttack(getAttack());
+    int trueDamage = target->getDefense() > rolledAttack ? 0 : rolledAttack - target->getDefense();
+    target->takeDamage(trueDamage);
+}
+
+void Player::takeDamage(int damage) {
+    setHealth(health - damage);
+    cout << YELLOW << "You have taken " << damage << " damage" << RESET << endl;
+    cout << GREEN << getName() << " has health points: " << getHealth() << RESET << endl;
+    if (health <= 0) {
+        cout<< RED << "You have died" << RESET << endl;
+    }
+}
+
+//Decide si el jugador puede huir
+void Player::flee(vector<Enemy *> enemies) {
+    std::sort(enemies.begin(), enemies.end(), compareSpeed);
+    Enemy *fastestEnemy = enemies[0];
+    bool fleed = false;
+    if (this->getSpeed() > fastestEnemy->getSpeed()) {
+        fleed = true;
+    } else {
+        srand(time(NULL));
+        int chance = rand() % 100;
+        cout << "chance: " << chance << endl;
+        fleed = chance > 50;
+    }
+
+    this->fleed = fleed;
+}
+
+void Player::emote() {
+    cout << "Jokes on you" << endl;
+}
+//Funcion para que player suba nivel
+void Player::levelUp() {
+    level++;
+    setHealth(getHealth() + 10);
+    setAttack(getAttack() + 5);
+    setDefense(getDefense() + 5);
+    setSpeed(getSpeed() + 5);
+}
+//Funcion para que player consiga experiencia
+void Player::gainExperience(int exp) {
+    experience += exp;
+    if (experience >= 100) {
+        levelUp();
+        experience = 0;
+    }
+}
+
+Character *Player::getTarget(vector<Enemy *> enemies) {
+    cout << BLUE << "\t [Choose a target]" << RESET << endl;
+    int targetIndex = 0;
+    for (int i = 0; i < enemies.size(); i++) {
+        cout << i+1 << ". " << enemies[i]->getName() << endl;
+    }
+    cin >> targetIndex;
+    //TODO: Add input validation
+    return enemies[targetIndex-1];
+}
+
+Action Player::takeAction(vector<Enemy *> enemies) {
+    int option = 0;
+    cout << BLUE << "\t [Choose an action]" << RESET << endl;
+    cout << "1. Attack" << endl;
+    cout << "2. Flee" << endl;
+    cin >> option;
+    Character *target = nullptr;
+
+    //Esta variable guarda
+    //1. Que voy a hacer?
+    //2. Con que velocidad/prioridad?
+    Action myAction;
+    //2.
+    myAction.speed = this->getSpeed();
+    myAction.subscriber = this;
+
+    switch (option) {
+        case 1:
+            target = getTarget(enemies);
+            myAction.target = target;
+            //1.
+            myAction.action = [this, target]() {
+                doAttack(target);
+            };
+            break;
+        case 2:
+            myAction.action = [this, enemies]() {
+                flee(enemies);
+            };
+            break;
+        default:
+            cout << RED << "Invalid option" << RESET << endl;
+            break;
+    }
+
+    return myAction;
+}
